ce5e37aef615603ecdf8d7daeceb84b2a28464a5
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-24 00:46:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,11 +1,11 @@
 class Song
-  attr_accessor :name
+  attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
   end
 
   def artist_name
-    @artist
+    self.artist.name
   end
 end
