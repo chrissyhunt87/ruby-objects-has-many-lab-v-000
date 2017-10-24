9287b84de76cb3812bc0063e57bcede707a028f9
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-24 00:39:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,7 +11,7 @@ class Artist
     song.artist = self
   end
 
-  def add_song_by_name(song)
+  def add_song_by_name(name)
     song = Song.new(name, artist)
     @songs << song
     song.artist = self
