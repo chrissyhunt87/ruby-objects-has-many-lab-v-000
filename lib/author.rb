bb3e19fad07d63c1017b7fe8e0fc735ac39d54b5
Chrissy Hunt <chrissy.hunt@gmail.com>
2017-10-24 00:51:06 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,28 @@
-class Author 
+class Author
   attr_accessor :name
 
   def initialize(name)
     @name = name
     @posts = []
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post = Post.new
+    @posts << post
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    @posts << post
+    post.author = self
+  end
+
+  def self.post_count
+    @@post_count
   end
 end
